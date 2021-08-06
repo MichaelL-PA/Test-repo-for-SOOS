8693e3e4c43d4f35b0fcec3bc100216077179a94
Michael Leonard <mleonard@packageaware.io>
2021-08-06 16:40:23 -0400

    entrypoint update

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -72,7 +72,7 @@ mkdir -p ${GITHUB_WORKSPACE}/soos/workspace
 source bin/activate
 
 # Get Package Aware CLI
-cd ${GITHUB_WORKSPACE}/package_aware/workspace
+cd ${GITHUB_WORKSPACE}/soos/workspace
 curl -s https://api.github.com/repos/soos-io/soos-ci-analysis-python/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | xargs -n 1 curl -LO
 sha256sum -c soos.sha256
 sha256sum -c requirements.sha256
